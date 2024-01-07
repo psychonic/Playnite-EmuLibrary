ab69741c93d7893567c2d53dc96b2231b0c92cc0
Alex Tiley <1131174+alextiley@users.noreply.github.com>
2024-01-07 02:38:32 +0000

    fix: clear roms upon uninstall

--- EmuLibrary/RomTypes/SingleFile/SingleFileUninstallController.cs
+++ EmuLibrary/RomTypes/SingleFile/SingleFileUninstallController.cs
@@ -28,6 +28,7 @@ namespace EmuLibrary.RomTypes.SingleFile
             {
                 _emuLibrary.Playnite.Dialogs.ShowMessage($"\"{Game.Name}\" does not appear to be installed. Marking as uninstalled.", "Game not installed", MessageBoxButton.OK);
             }
+            Game.Roms.Clear();
             InvokeOnUninstalled(new GameUninstalledEventArgs());
         }
     }
