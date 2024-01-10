ffc0ff94599143ce3d3c519d51f21c878ae3fc63
Alex Tiley <1131174+alextiley@users.noreply.github.com>
2024-01-10 02:19:49 +0000

    fix: destination paths not including filenames when copying via windows

--- EmuLibrary/Util/FileCopier/WindowsFileCopier.cs
+++ EmuLibrary/Util/FileCopier/WindowsFileCopier.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.VisualBasic.FileIO;
 using System;
 using System.IO;
-using System.Linq;
-using System.Security.Cryptography;
 
 namespace EmuLibrary.Util.FileCopier
 {
@@ -17,48 +15,31 @@ namespace EmuLibrary.Util.FileCopier
                 if (Source is DirectoryInfo)
                 {
                     FileSystem.CopyDirectory(Source.FullName, Destination.FullName, UIOption.AllDialogs);
+                    return;
                 }
-                else
-                {
-                    FileSystem.CopyFile(Source.FullName, Destination.FullName, UIOption.AllDialogs);
-                }
+                FileSystem.CopyFile(Source.FullName, Path.Combine(Destination.FullName, Source.Name), UIOption.AllDialogs);
             }
             catch (Exception ex)
             {
                 try
                 {
-                    OnCopyDialogClosedByUser();
+                    // For directories, some child nodes may have been partially copied before cancellation. Clean these up.
+                    if (Source is DirectoryInfo)
+                    {
+                        FileSystem.DeleteDirectory(Destination.FullName, UIOption.OnlyErrorDialogs, RecycleOption.DeletePermanently);
+                    }
+                    // Remove the file if for some reason it still exists after user cancellation.
+                    else if (Source is FileInfo)
+                    {
+                        FileSystem.DeleteFile(Destination.FullName, UIOption.OnlyErrorDialogs, RecycleOption.DeletePermanently);
+                    }
                 }
                 catch { }
                 if (ex is OperationCanceledException)
                 {
                     throw new WindowsCopyDialogClosedException("The user cancelled the copy request", ex);
                 }
-                throw new Exception($"Unable to copy source {Source.FullName} to {Destination.FullName}", ex);
-            }
-        }
-
-
-        private void OnCopyDialogClosedByUser()
-        {
-            // For directories, some child nodes may have been partially copied before cancellation. Clean these up.
-            if (Source is DirectoryInfo)
-            {
-                // Since this is a directory, some of child nodes may have been copied successfully before cancellation.
-                // Let's remove them.
-                FileSystem.DeleteDirectory(Destination.FullName, UIOption.OnlyErrorDialogs, RecycleOption.DeletePermanently);
-            }
-            // Remove the file if for some reason it still exists after user cancellation.
-            // Also ensure that we clean up any empty directories that were added during copy.
-            else if (Source is FileInfo)
-            {
-                FileSystem.DeleteFile(Destination.FullName, UIOption.OnlyErrorDialogs, RecycleOption.DeletePermanently);
-
-                var parent = Directory.GetParent(Destination.FullName);
-                if (parent != null && !Directory.EnumerateFileSystemEntries(parent.FullName).Any())
-                {
-                    FileSystem.DeleteDirectory(parent.FullName, UIOption.OnlyErrorDialogs, RecycleOption.DeletePermanently);
-                }
+                throw new Exception($"Unable to copy source \"{Source.FullName}\" to destination \"{Destination.FullName}\"", ex);
             }
         }
     }
